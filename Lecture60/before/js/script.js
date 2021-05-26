c51c8780e19bcd9b67de3947a135b24d300e9b9b
Geetakrishna <geethu4444@gmail.com>
2021-05-27 00:00:52 +0530

    initial commit for assignment5

---
+++ Lecture60/before/js/script.js
@@ -0,0 +1,147 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    let screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+
+
+
+(function (global) {
+  let dc = {};
+  let homeHtml = "snippets/home-snippet.html";
+  let allCategoriesUrl = 
+  "https://davids-restaurant.herokuapp.com/categories.json";
+  let categoriesTitleHtml = "snippets/categories-title-snippet.html";
+  let categoryHtml = "snippets/category-snippet.html";
+  let menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  let menuItemsTitleHtml = "snippets/menu-items-title.html";
+  let menuItemsHtml = "snippets/menu-items.html";
+
+  let insertHtml = function (selector, html) {
+    let targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+  let showLoading = function(selector) {
+    let html = '<div class="text-center">';
+    html += '<img src="images/ajax-loader.gif></div>';
+    insertHtml(selector, html);
+  }
+
+  let insertProperty = 
+  function (string, propName, propValue) {
+    let propToReplace = 
+    "{{" + propName + "}}";
+    string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  };
+
+  document.addEventListener("DOMContentLoaded", function (event) {
+    showLoading('#main-content');
+    $ajaxUtils.sendGetRequest(
+      homeHtml, 
+      function (responseText) {
+        document.querySelector("#main-content")
+        .innerHTML = responseText;
+      },
+      false);
+  });
+
+  dc.loadMenuCategories = function () {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      allCategoriesUrl,
+       buildAndShowCategoriesHTML);
+  };
+
+  function buildAndShowCategoriesHTML (categories) {
+    $ajaxUtils.sendGetRequest(
+      categoriesTitleHtml,
+       function (categoriesTitleHtml) {
+        $ajaxUtils.sendGetRequest(
+          categoryHtml,
+          function (categoryHtml) {
+            let categoriesViewHtml = 
+            buildCategoriesViewHtml(categories,
+              categoriesTitleHtml,
+              categoryHtml);
+            insertHtml("#main-content", categoriesViewHtml);
+          },
+          false);
+      },
+      false);
+  };
+
+  function buildCategoriesViewHtml(categories,
+    categoriesTitleHtml, 
+    categoryHtml) {
+    let finalHtml = 
+    categoriesTitleHtml;
+    finalHtml += "<section class='row'>";
+
+    for (let i = 0; i < categories.length; i++) {
+      let html =
+       categoryHtml;
+      let name = 
+      "" + categories[i].name;
+      let short_name =
+       categories[i].short_name;
+      html = 
+      insertProperty(html, "name", name);
+      html = 
+      insertProperty(html, 
+        "short_name", 
+        short_name);
+      finalHtml += html;
+    }
+    finalHtml += "</section>";
+    return finalHtml;
+  };
+
+
+  dc.loadMenuItems = function (categoryShort) {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      menuItemsUrl + categoryShort,
+      buildAndShowMenuItemsHTML
+    );
+  };
+
+  function BuildAndShowMenuItemsHtml (categoryMenuItems) {
+    $ajaxUtils.sendGetRequest(
+      menuItemsTitleHtml,
+      function(menuItemsTitleHtml) {
+        $ajaxUtils.sendGetRequest(
+          menuItemsHtml, 
+          function (menuItemsHtml) {
+            let menuItemsViewHtml = 
+              buildMenuItemsViewHtml(categoryMenuItems,
+                                      menuItemsTitleHtml,
+                                      menuItemsHtml);
+              insertHtml("#main-content", menuItemsViewHtml);
+          },
+          false);
+      },
+      false);
+  };
+
+
+  global.$dc = dc;
+})(window);
--- assignment4/script.js
+++ assignment4/script.js
@@ -50,14 +50,15 @@ for (let idx in names) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  var firstLetter = name.charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (name.charAt(0).toLowerCase() == 'j') {
+ 
+  if ( firstLetter == 'j') {
     // byeSpeaker.xxxx
     this.byeSpeaker.speak(name);
   } else {
